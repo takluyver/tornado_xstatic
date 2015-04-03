5864639fbf7ae6015ea55e0f796b6f23be7250a2
kAlmAcetA <kalmaceta@gmail.com>
2015-04-03 12:02:48 +0200

    update example

--- example.py
+++ example.py
@@ -1,18 +1,20 @@
 import tornado.ioloop
 import tornado.web
-import tornado_xstatic
+from tornado_xstatic import XStaticFileHandler, xstatic_ui_method
+
 
 class MyHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("eg_template.html",
-                    xstatic=self.application.settings['xstatic_url'])
+        self.render("eg_template.html")
+
 
 if __name__ == "__main__":
-    application = tornado.web.Application([
-        (r"/", MyHandler),
-        (r"/xstatic/(.*)", tornado_xstatic.XStaticFileHandler,
-            {"allowed_modules": ["jquery"]}),
-    ], xstatic_url=tornado_xstatic.url_maker("/xstatic/")
+    application = tornado.web.Application(
+        [
+            (r"/", MyHandler),
+            (r"/xstatic/(.*)", XStaticFileHandler, {"allowed_modules": ["jquery"]}),
+        ],
+        ui_methods={'xstatic': xstatic_ui_method('/xstatic/')}
     )
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
